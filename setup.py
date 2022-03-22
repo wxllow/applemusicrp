f81f80defae967651ff8c25120f8457346bcea71
Willow <willowjl1117@gmail.com>
2022-03-22 18:46:25 -0400

    Fix dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
+import platform
 
+# py2app
 APP = ['applemusicrp.py']
 DATA_FILES = [('scripts', ['scripts/']), ('assets', ['assets/'])]
 OPTIONS = {
@@ -8,12 +10,21 @@ OPTIONS = {
         'CFBundleShortVersionString': '1.1.0',
         'LSUIElement': True,
     },
-    'packages': ['rumps', 'pypresence', 'infi.systray'],
+    'packages': ['rumps', 'pypresence'],
 }
+
+install_dependencies = ['pypresence']
+
+if platform.system() == 'Darwin':
+    install_dependencies.append('rumps')
+elif platform.system() == 'Windows':
+    install_dependencies.append('pywin32')
+    install_dependencies.append('infi.systray')
 
 setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'], install_requires=['rumps', 'pypresence']
+    setup_requires=['py2app'],
+    install_requires=install_dependencies
 )
