f5ffa3ce797e79974daecae8305448c19e33c488
wxllow <35239049+wxllow@users.noreply.github.com>
2022-04-01 00:11:07 -0400

    Even better errors :p

--- applemusicrp.py
+++ applemusicrp.py
@@ -35,6 +35,7 @@ if ostype == 'Darwin':
     else:
         macos_legacy = False
 elif ostype == 'Windows':
+    # Windows needs a lot of dependencies :p
     from infi.systray import SysTrayIcon
     import win32com.client
     import pythoncom
@@ -43,6 +44,7 @@ elif ostype == 'Windows':
     systray = SysTrayIcon(os.path.join(os.path.dirname(os.path.realpath(
         __file__)), 'assets/icon.ico'), "AppleMusicRP", ())
 else:
+    # There isn't iTunes for Linux :(
     dialite.fail("AppleMusicRP", "You need to be using Windows or macOS!")
     exit(1)
 
@@ -51,7 +53,9 @@ try:
     RPC.connect()
 except (ConnectionRefusedError, pypresence.exceptions.DiscordNotFound, pypresence.exceptions.DiscordError) as e:
     # Needs to be replaced with cross-platform error dialog
-    dialite.fail("AppleMusicRP", "Could not connect to Discord!")
+    msg = 'Could not connect to Discord!'
+    logging.exception(msg)
+    dialite.fail("AppleMusicRP", msg)
     exit(1)
 
 
@@ -71,6 +75,7 @@ def get_music_info():
 
         return p.stdout.read().decode("utf-8").strip().split('\\')
     else:
+        # Check if iTunes is running
         if "iTunes.exe" in (p.name() for p in psutil.process_iter()):
             itunes = win32com.client.Dispatch(
                 "iTunes.Application", pythoncom.CoInitialize())
@@ -84,36 +89,55 @@ def get_music_info():
             return ['STOPPED']
 
         return [('PAUSED' if playerstate == 0 else 'PLAYING'), current_track.Name, current_track.Artist,
-                current_track.Album, str(itunes.PlayerPosition)]  # Test
+                current_track.Album, str(itunes.PlayerPosition)]
 
 
 def rp_updater():
-    while True:
-        # info[0] is status (PLAYING, PAUSED, or STOPPED), info[1] is title, info[2] is artist, info[3] is album
-        info = get_music_info()
+    err_count = 0
 
-        if info[0] == "PLAYING" or info[0] == "PAUSED":
-            RPC.update(large_image="logo",
-                       large_text='Using AppleMusicRP :)',
-                       details=f'{"Playing" if info[0] == "PLAYING" else "Paused"} {info[1]}',
-                       state=f'By {info[2]} on {info[3]}',
-                       start=(
-                           (time.time()-int(float(info[4].split(',')[0]))) if info[0] == "PLAYING" else None))
-        else:
-            RPC.clear()
+    while True:
+        try:
+            # info[0] is status (PLAYING, PAUSED, or STOPPED), info[1] is title, info[2] is artist, info[3] is album, info[4] is player position
+            info = get_music_info()
+
+            if info[0] == "PLAYING" or info[0] == "PAUSED":
+                # .split(',')[0] is an attempt to fix issue #5
+                elapsed = int(float(info[4].split(',')[0]))
+
+                RPC.update(large_image="logo",
+                           large_text='Using AppleMusicRP :)',
+                           details=f'{"Playing" if info[0] == "PLAYING" else "Paused"} {info[1]}',
+                           state=f'By {info[2]} on {info[3]}',
+                           start=(
+                               (time.time()-elapsed) if info[0] == "PLAYING" else None))
+            else:
+                RPC.clear()
+        except:
+            err_count += 1
+
+            msg = 'An unexpected error has occured while trying to update your Discord status!'
+            logging.exception(msg)
+            dialite.fail("AppleMusicRP", msg)
+
+            # Prevent endless loops of errors!
+            if err_count > 1:
+                exit(1)
 
         time.sleep(1)
 
 
 if ostype == 'Darwin':
+    # Just needs a quit button, no need to define anything here for now
     class App(rumps.App):
         pass
 
 
 if __name__ == '__main__':
+    # Launch Rich Presence (RP) updating thread
     x = threading.Thread(target=rp_updater, daemon=True)
     x.start()
 
+    # Tray/menu bar app
     if ostype == 'Darwin':
         app = App('🎵')
         exit(app.run())
