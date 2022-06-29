48da5bcbd27f401f49325ac0002c967845958c58
wxllow <willow@wxllow.dev>
2022-06-29 13:00:07 -0400

    Atomic write config

--- src/applemusicrp/config.py
+++ src/applemusicrp/config.py
@@ -1,9 +1,10 @@
 import os
 import toml
 import logging
+import shutil
+import pathlib
 
 from appdirs import AppDirs
-import dialite
 
 dirs = AppDirs("AppleMusicRP", "wxllow")
 
@@ -15,6 +16,8 @@ class Config:
     def __init__(self) -> None:
         self._loc = os.path.join(dirs.user_data_dir, "config.toml")
 
+        pathlib.Path(os.path.dirname(self._loc)).mkdir(parents=True, exist_ok=True)
+        
         if not os.path.exists(self._loc):
             with open(self._loc, "w") as f:
                 pass
@@ -22,6 +25,7 @@ class Config:
         try:
             self._config = toml.load(self._loc)
         except Exception as e:
+            logging.exception(e)
             logging.error("Config file corrupt, resetting config.")
             self._config = {}
             self.save()
@@ -31,5 +35,9 @@ class Config:
         return self._config
 
     def save(self):
-        with open(self._loc, "w") as f:
+        # Atomic save (Prevents corruption of config file on crash/quit)
+        with open(f"{self._loc}.tmp", "w") as f:
             toml.dump(self._config, f)
+        
+        shutil.move(f"{self._loc}.tmp", self._loc)      
+        
